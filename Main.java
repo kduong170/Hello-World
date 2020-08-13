53c62fb22ad4e787cbef8dc3a95aec2c6604c174
kduong170 <69610576+kduong170@users.noreply.github.com>
2020-08-12 17:55:56 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I like to play videogames and do track.");
+		System.out.println("I recently played Cuphead and Breath of the Wild.");
+		System.out.println("I do long jump, triple jump, and high jump in track.");
+		System.out.println("I play musical instruments as well.");
+		System.out.println("I currently play the violin but I have also played the saxophone in band before.");
+	}
+}
